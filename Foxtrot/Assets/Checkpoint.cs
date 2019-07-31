1d988facd7809cb4bb4885a42f9e5b507511604e
SnideAnteater <totsmcgeee@gmail.com>
2019-07-31 11:50:26 +0800

    Massive Updates

---
+++ Foxtrot/Assets/Checkpoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public GameObject checkpoint1;
+    public GameObject checkpoint2;
+    public GameObject checkpoint3;
+    public GameObject checkpoint4;
+    public GameObject checkpoint5;
+    public GameObject checkpoint6;
+    public GameObject checkpoint7;
+    public GameObject checkpoint8;
+    public GameObject checkpoint9;
+    public GameObject checkpoint10;
+
+
+    
+}
---
+++ Foxtrot/Assets/Objective.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective : MonoBehaviour
+{
+    public Material activeRing;
+    public Material inactiveRing;
+    public Material finalRing;
+    public GameObject ring1;
+    public GameObject ring2;
+    public GameObject ring3;
+    public GameObject ring4;
+    public GameObject ring5;
+
+   
+}
